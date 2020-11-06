000808919990925696949a75329fa5a7c99c7a7e
Rajendra Choudhary <rchoudhary157@gmail.com>
2020-11-07 01:42:05 +0530

    adding product section

--- resources/script.js
+++ resources/script.js
@@ -1,18 +1,19 @@
 const newSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+  const burger = document.querySelector('.burger')
+  const nav = document.querySelector('.nav-links')
+  const navLinks = document.querySelectorAll('.nav-links li')
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = "";
-            } else {
-                link.style.animation = `navLinkFade 0.2s ease forwards ${index/5 + 0.2}s`;
-            }
-        });
-        burger.classList.toggle('toggle');
-    });
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active')
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ''
+      } else {
+        link.style.animation = `navLinkFade 0.2s ease forwards ${index / 5 +
+          0.2}s`
+      }
+    })
+    burger.classList.toggle('toggle')
+  })
 }
-newSlide();
+newSlide()
